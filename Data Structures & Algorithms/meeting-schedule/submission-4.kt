f965a3c9938a85969840fa6042c1e93ae09abd0d
Paweł <48386403+Onxi95@users.noreply.github.com>
2026-05-14 21:08:50 +0200

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.kt
@@ -0,0 +1,18 @@
+/**
+ * Definition of Interval:
+ * class Interval(var start: Int, var end: Int) {}
+ */
+
+class Solution {
+    fun canAttendMeetings(intervals: List<Interval>): Boolean {
+        val sortedIntervals = intervals.sortedBy { it.start }
+
+        for(i in 0 until sortedIntervals.size - 1) {
+            val current = sortedIntervals[i]
+            val next = sortedIntervals[i + 1]
+
+            if (current.end > next.start) return false
+        }
+        return true
+    }
+}
