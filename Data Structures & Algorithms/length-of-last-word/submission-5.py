4137f5efa2ccfb31a4a1498b97d005f69ae774c1
Paweł <48386403+Onxi95@users.noreply.github.com>
2026-09-02 21:02:16 +0200

    Add: length-of-last-word - submission-5

---
+++ Data Structures & Algorithms/length-of-last-word/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        right = len(s) - 1
+        total = 0
+        while right > 0 and s[right] == " ":
+            right -= 1
+
+        while right > 0 and s[right] != " ":
+            right -= 1
+            total += 1
+
+        return total if right != len(s) - 1 else len(s)
