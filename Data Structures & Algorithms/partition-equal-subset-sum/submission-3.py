c4ee76d1c4ad1eeea4b0eea4a636c7afce794987
Paweł <48386403+Onxi95@users.noreply.github.com>
2026-09-08 15:40:45 +0200

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total_sum = sum(nums)
+        if total_sum % 2 != 0:
+            return False
+        half_sum = total_sum // 2
+
+        memo = {}
+
+        def dfs(index: int, reminder: int):
+            if reminder == 0:
+                return True
+            if index >= len(nums) or reminder < 0:
+                return False
+
+            key = f"{index}-{reminder}"
+            if key in memo:
+                return memo[key]
+
+            left = dfs(index + 1, reminder - nums[index])
+            right = dfs(index + 1, reminder)
+
+            result = left or right
+            memo[key] = result
+
+            return result
+
+        return dfs(0, half_sum)
+            
